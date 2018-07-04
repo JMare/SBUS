8ae800ab29f0c643652b17118e2ef7a993b24768
James Mare <git@jmare.me>
2018-07-04 15:13:53 +1000

    Customise scaling in constructor and add constraining

--- SBUS.h
+++ SBUS.h
@@ -6,19 +6,19 @@ brian.taylor@bolderflight.com
 
 Copyright (c) 2016 Bolder Flight Systems
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
-and associated documentation files (the "Software"), to deal in the Software without restriction, 
-including without limitation the rights to use, copy, modify, merge, publish, distribute, 
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or 
+The above copyright notice and this permission notice shall be included in all copies or
 substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
-BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
@@ -29,16 +29,16 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 
 class SBUS{
 	public:
-    	SBUS(HardwareSerial& bus);
+    	SBUS(HardwareSerial& bus, uint16_t sbusMin , uint16_t sbusMax);
     	void begin();
     	bool read(uint16_t* channels, uint8_t* failsafe, uint16_t* lostFrames);
     	bool readCal(float* calChannels, uint8_t* failsafe, uint16_t* lostFrames);
     	void write(uint16_t* channels);
   	private:
   		uint8_t _fpos;
-        const uint16_t SBUS_TIMEOUT = 10000;
-  		const float _sbusScale = 0.00122025625f;
-  		const float _sbusBias = -1.2098840f;
+      const uint16_t SBUS_TIMEOUT = 10000;
+  		float _sbusScale;
+  		float _sbusBias;
   		const uint8_t _sbusHeader = 0x0F;
   		const uint8_t _sbusFooter = 0x00;
       const uint8_t _sbus2Footer = 0x04;
@@ -47,7 +47,7 @@ class SBUS{
   		static const uint8_t _payloadSize = 24;
   		uint8_t _payload[_payloadSize];
   		HardwareSerial* _bus;
-  		
+
   		bool parse();
 };
 
